05fba8a64fefaf6ad2be9c8bbbd05df0699f6da6
Cristiano Moraes Oliveira <cmoraes.ti@gmail.com>
2019-07-09 16:34:24 -0300

    Updated Server Component

--- frontend/NgSightFrontEnd/src/app/server/server.component.ts
+++ frontend/NgSightFrontEnd/src/app/server/server.component.ts
@@ -9,10 +9,32 @@ import { Server } from '../shared/server'
 export class ServerComponent implements OnInit {
 
   constructor() { }
-  
+
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit() {
+    this.setServerAction(this.serverInput.isOnline);
+  }
+
+  setServerAction(isOnline: boolean) {
+    if(isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66bb6a';
+      this.buttonText = 'Shut Down';
+      
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6b6b';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.setServerAction(!onlineStatus);
   }
 
 }
