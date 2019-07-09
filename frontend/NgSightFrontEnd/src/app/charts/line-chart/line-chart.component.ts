c39e0dc0d7dd8b4ee61fd2423fffb3101cfe9a56
Cristiano Moraes Oliveira <cmoraes.ti@gmail.com>
2019-07-08 22:29:31 -0300

    Creating Sample Data for linechart

--- frontend/NgSightFrontEnd/src/app/charts/line-chart/line-chart.component.ts
+++ frontend/NgSightFrontEnd/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../../shared/chartcolors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analisys' },
+  { data: [12, 14, 46, 23, 38, 56], label: 'Image Recognition' },
+  { data: [32, 14, 44, 53, 58, 66], label: 'Forecasting' }
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +17,19 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+
+  };
+  
+  lineChartLegend = true;
+  lineChartType = 'line';
+
+  lineChartColors = LINE_CHART_COLORS;
+
 
   ngOnInit() {
   }
