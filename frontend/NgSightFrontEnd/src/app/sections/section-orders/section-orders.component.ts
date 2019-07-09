21700522ccecf962709057b7f5508e83d3d4848a
Cristiano Moraes Oliveira <cmoraes.ti@gmail.com>
2019-07-08 23:24:17 -0300

    Creating Orders table

--- frontend/NgSightFrontEnd/src/app/sections/section-orders/section-orders.component.ts
+++ frontend/NgSightFrontEnd/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../../shared/orders'
 
 @Component({
   selector: 'app-section-orders',
@@ -7,7 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionOrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  orders: Order[] = [
+    {
+      id: 1, 
+      customer: {
+        id: 1, 
+        name: 'asdf', 
+        state: 'CO', 
+        email: 'sss@sss.com'
+      }, 
+      total: 10, 
+      placed: new Date('2019-06-03'), 
+      fulfilled: new Date('2019-07-03'), 
+      status: 'Completed'
+    },
+    {
+      id: 2, 
+      customer: {
+        id: 1, 
+        name: 'asdf', 
+        state: 'CO', 
+        email: 'sss@sss.com'
+      }, 
+      total: 10, 
+      placed: new Date('2019-06-03'), 
+      fulfilled: new Date('2019-07-03'), 
+      status: 'Completed'
+    },
+    {
+      id: 3, 
+      customer: {
+        id: 1, 
+        name: 'asdf', 
+        state: 'CO', 
+        email: 'sss@sss.com'
+      }, 
+      total: 10, 
+      placed: new Date('2019, 12, 1'), 
+      fulfilled: new Date('2019, 12, 3'), 
+      status: 'Completed'
+    },
+    {
+      id: 4, 
+      customer: {
+        id: 1, 
+        name: 'asdf', 
+        state: 'CO', 
+        email: 'sss@sss.com'
+      }, 
+      total: 10, 
+      placed: new Date('2019-06-03'), 
+      fulfilled: new Date('2019-07-03'), 
+      status: 'Completed'
+    }
+  ];
 
   ngOnInit() {
   }
