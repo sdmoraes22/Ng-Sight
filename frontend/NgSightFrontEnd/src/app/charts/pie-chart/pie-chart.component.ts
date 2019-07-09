b8637c8715c4cebae7b3c4986b91b2cb1b82a434
Cristiano Moraes Oliveira <cmoraes.ti@gmail.com>
2019-07-08 21:25:08 -0300

    Add pie chart component to the project

--- frontend/NgSightFrontEnd/src/app/charts/pie-chart/pie-chart.component.ts
+++ frontend/NgSightFrontEnd/src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,18 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 450, 120];
+  pieChartLabels: string[] = ['XYZ', 'Main St', 'Acme'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#000000'
+      
+    }
+  ];
+
+  pieChartType = 'pie';
+
   ngOnInit() {
   }
 
