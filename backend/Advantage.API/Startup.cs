bff05d2d90cc5b9c46a64e7cbac7469daef26d75
Cristiano Moraes Oliveira <cmoraes.ti@gmail.com>
2019-07-09 22:02:30 -0300

    Initial Setup for the project and Database with Entity Framework Core and user-secrets to setup the connection string

--- backend/Advantage.API/Startup.cs
+++ backend/Advantage.API/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Advantage.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -15,6 +17,7 @@ namespace Advantage.API
 {
     public class Startup
     {
+        private string _connectionString = null;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +28,11 @@ namespace Advantage.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            _connectionString = Configuration["secretConnectionString"];
+            
+            services.AddEntityFrameworkNpgsql()
+                    .AddDbContext<ApiContext>(opt => opt
+                        .UseNpgsql(_connectionString));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
--- backend/Advantage.API/obj/Debug/netcoreapp2.2/Advantage.API.AssemblyInfo.cs
+++ backend/Advantage.API/obj/Debug/netcoreapp2.2/Advantage.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("Advantage-Api-Secrets")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advantage.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
